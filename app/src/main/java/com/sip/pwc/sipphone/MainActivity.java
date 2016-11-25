c6d2e0d22251644c7b4144ec1cb6c773c47b7df8
Vergil Yang <13693292428@163.com>
2016-11-25 19:30:52 +0800

    Initial commit

---
+++ app/src/main/java/com/sip/pwc/sipphone/MainActivity.java
@@ -0,0 +1,20 @@
+package com.sip.pwc.sipphone;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.sip.pwc.sipphone.api.SipManager;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent prefsIntent = new Intent(SipManager.ACTION_UI_PREFS_FAST);
+        prefsIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(prefsIntent);
+    }
+}
